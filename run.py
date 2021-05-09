236686b0220cb7f2cfca726fa1b8cc96891a0aa7
smcgdub <stephen_xyz1@hotmail.com>
2021-05-09 13:56:06 +0000

    Adjusted validation to check all values were intergers

--- run.py
+++ run.py
@@ -21,6 +21,25 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try converts all string values to intergers.
+    Raises ValueError if strings cannot be converted into 
+    integers or if there's not exactly six values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+              f"exactly 6 values are required you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+
 
 get_sales_data()
